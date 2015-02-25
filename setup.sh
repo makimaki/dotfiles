706481a2f6b8000e13e405766e71197d90ec403b
Yuta Sakamaki <an0nym0us@makimaki.jp>
2015-02-25 16:58:54 +0900

    - using jenv for java. - supports for private github repository.

--- setup.sh
+++ setup.sh
@@ -7,6 +7,10 @@ BASH=`which bash`
 # 1階層目のディレクトリを取得
 DIRS=($(find . -type d -maxdepth 1 -mindepth 1 | grep -v "\/\."))
 
+# Submodule
+git submodule init
+git submodule update
+
 # ディレクトリ毎のセットアップスクリプトを実行
 for d in ${DIRS[@]}
 do
