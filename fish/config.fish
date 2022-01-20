b8dfeff4226e81ebcd17b3acaf3e3279ddd6d9b5
Yuta Sakamaki <an0nym0us@makimaki.jp>
2022-01-20 15:57:23 +0900

    update

--- fish/config.fish
+++ fish/config.fish
@@ -3,12 +3,21 @@ if type direnv >/dev/null 2>&1
   eval (direnv hook fish)
 end
 
+# gcloud
+source "/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.fish.inc"
+
+# asdf
+source (brew --prefix asdf)/asdf.fish
+
 # anyenv
-status --is-interactive; and source (anyenv init -|psub)
+#status --is-interactive; and source (anyenv init -|psub)
 
 # pyenv
 #status --is-interactive; and source (pyenv init -|psub)
-status --is-interactive; and source (pyenv virtualenv-init -|psub)
+#status --is-interactive; and source (pyenv virtualenv-init -|psub)
+status is-login; and pyenv init --path | source
+pyenv init - | source
+status --is-interactive; and pyenv virtualenv-init - | source
 
 # nodenv
 #status --is-interactive; and source (nodenv init -|psub)
@@ -31,47 +40,67 @@ end
 set fish_custom $HOME/.config/fish/custom
 
 # Load oh-my-fish configuration.
-. $fish_path/oh-my-fish.fish
+#. $fish_path/oh-my-fish.fish
 
 # Theme
-Theme "bobthefish"
+#Theme "bobthefish"
+set -g theme_display_date yes
 set -g theme_display_git yes
 set -g theme_display_hg yes
 set -g theme_display_virtualenv yes
-set -g theme_display_ruby yes
-set -g theme_display_user yes
-set -g default_user your_normal_user
+set -g theme_display_ruby no
+set -g theme_display_k8s_context yes
+set -g theme_powerline_fonts yes
+set -g theme_nerd_fonts no
+set -g theme_show_exit_status yes
+set -g theme_display_jobs_verbose yes
+set -g theme_date_timezone Asia/Tokyo
+set -g theme_display_user ssh
+set -g theme_date_format "+%Y-%m-%dT%H:%M:%S (%a)"
 
 # Which plugins would you like to load? (plugins can be found in ~/.oh-my-fish/plugins/*)
 # Custom plugins may be added to ~/.oh-my-fish/custom/plugins/
 # Example format: set fish_plugins autojump bundler
 
-Plugin "android-sdk"
-Plugin "archlinux"
-Plugin "balias"
-#Plugin "better-alias"
-Plugin "brew"
-Plugin "bundler"
-Plugin "cask"
-Plugin "emoji-clock"
-Plugin "export"
-Plugin "extract"
-Plugin "gem"
-Plugin "getopts"
-Plugin "gi"
-Plugin "local-config"
-Plugin "msg"
-Plugin "osx"
-Plugin "php"
-#Plugin "pyenv"
-Plugin "python"
-Plugin "rails"
-#Plugin "rbenv"
-Plugin "replace"
-Plugin "ssh"
-Plugin "tmux"
-Plugin "theme"
+#Plugin "android-sdk"
+#Plugin "archlinux"
+#Plugin "balias"
+##Plugin "better-alias"
+#Plugin "brew"
+#Plugin "bundler"
+#Plugin "cask"
+#Plugin "emoji-clock"
+#Plugin "export"
+#Plugin "extract"
+#Plugin "gem"
+#Plugin "getopts"
+#Plugin "gi"
+#Plugin "local-config"
+#Plugin "msg"
+#Plugin "osx"
+#Plugin "php"
+##Plugin "pyenv"
+#Plugin "python"
+#Plugin "rails"
+##Plugin "rbenv"
+#Plugin "replace"
+#Plugin "ssh"
+#Plugin "tmux"
+#Plugin "theme"
 
+fzf_configure_bindings --directory=\cf
 
 set -g fish_user_paths "/usr/local/opt/curl/bin" $fish_user_paths
+set -g fish_user_paths "$HOME/.cargo/bin" $fish_user_paths
+
+set -gx LDFLAGS "-L/usr/local/opt/openblas/lib -L/usr/local/opt/lapack/lib -L/usr/local/opt/zlib/lib -L/usr/local/opt/bzip2/lib"
+set -gx CPPFLAGS "-I/usr/local/opt/openblas/include -I/usr/local/opt/lapack/include -I/usr/local/opt/zlib/include -I/usr/local/opt/bzip2/include"
+
+if test -d (brew --prefix)"/share/fish/completions"
+    set -gx fish_complete_path $fish_complete_path (brew --prefix)/share/fish/completions
+end
+
+if test -d (brew --prefix)"/share/fish/vendor_completions.d"
+    set -gx fish_complete_path $fish_complete_path (brew --prefix)/share/fish/vendor_completions.d
+end
 
--- fish/env.fish
+++ fish/env.fish
@@ -8,6 +8,8 @@ export LESSCHARSET=utf-8
 append-to-path $HOME/bin
 append-to-path $HOME/dotfiles/bin
 
+set -x RUSTC_WRAPPER (which sccache)
+
 #export GOPATH=$HOME/golang
 #export GOBIN=$GOPATH/bin
 #export GOROOT=(go env GOROOT)
@@ -19,14 +21,19 @@ append-to-path (go env GOPATH)/bin
 # vim
 #export EDITOR=/Applications/MacVim.app/Contents/MacOS/Vim
 #alias vi='env LANG=ja_JP.UTF-8 /Applications/MacVim.app/Contents/MacOS/Vim'
-alias vim='env LANG=ja_JP.UTF-8 /Applications/MacVim.app/Contents/MacOS/Vim'
+#alias vim='env LANG=ja_JP.UTF-8 /Applications/MacVim.app/Contents/MacOS/Vim'
 
 # NeoVim
 export EDITOR=/usr/local/bin/nvim
 alias vi='env LANG=ja_JP.UTF-8 /usr/local/bin/nvim'
+alias vim='env LANG=ja_JP.UTF-8 /usr/local/bin/nvim'
+
+#export DOCKER_HOST=ssh://dockerhost
 
 export http_proxy='http://10.63.0.11:8080'
 export https_proxy=$http_proxy
 export HTTP_PROXY=$http_proxy
 export HTTPS_PROXY=$http_proxy
-export no_proxy='*.local,169.254/16,10.0.0.0/8,127.0.0.1,localhost,rancher.makimaki.jp,k8s.makimaki.jp'
+export no_proxy='*.local,172.16.0.0/12,169.254.0.0/16,10.0.0.0/8,127.0.0.1,localhost,rancher.makimaki.jp,k8s.makimaki.jp,minio'
+#export no_proxy='localhost,127.0.0.1'
+export NO_PROXY=$no_proxy
